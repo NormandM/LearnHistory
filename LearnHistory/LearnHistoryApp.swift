705b52924e2957220ac0e90f93824bfd464de506
NormandM <nmartin1956@gmail.com>
2022-01-04 11:56:52 -0500

    Initial Commit

---
+++ LearnHistory/LearnHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnHistoryApp.swift
+//  LearnHistory
+//
+//  Created by Normand Martin on 2022-01-04.
+//
+
+import SwiftUI
+
+@main
+struct LearnHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
